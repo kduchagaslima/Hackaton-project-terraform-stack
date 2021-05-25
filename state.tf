c1ebc972b91f452582e3c79d9100d68dbcff2a9c
Carlos E Lima <kduchagas.lima@gmail.com>
2021-05-24 21:55:26 -0300

    Adjust bucket name

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-5dvp-grupo2"
+    bucket = "hackathon-fiap-5dvp-grupo2-educapp"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
